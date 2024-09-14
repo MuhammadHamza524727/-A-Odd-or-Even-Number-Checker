020d279e6235a98cf796107326e017c0711e0a81
MuhammadHamza524727 <muhammadhamza524727@gmail.com>
2024-09-14 11:41:44 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+ // Prompt the user to enter a number
+ let num = parseInt(prompt("Enter a number:"));
+
+ // Check if the number is odd or even using the modulo operator
+ if (num % 2 === 0) {
+     alert("The number " + num + " "+ "is even.")
+ } else {
+     alert("The number " + num + " "+ "is odd.")
+
+ }
+
+ // Increment the number by 1 using the ++ operator
+ num++;
+
+
+
+ // Display the new value using an alert
+ alert("After Incrementing, the number is "+" " + num + ".")
